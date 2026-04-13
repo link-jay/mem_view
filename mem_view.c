0a21f22c0247a1e9bcfafc77fc8bc23a5ca76b98
li huijie <li.huijie.lhj@qq.com>
2026-04-13 17:54:09 +0800

    统一buf释放逻辑

--- mem_view.c
+++ mem_view.c
@@ -18,6 +18,19 @@ struct winsize* get_tsz()
   return ws;
 }
 
+struct mem_buf {
+  struct heap_stack* mem_info;
+  char* old_buf;
+  char* new_buf;
+};
+
+void free_buf(struct mem_buf* buf)
+{
+  free(buf->mem_info);
+  free(buf->new_buf);
+  free(buf);
+}
+  
 void draw_view(size_t rows, size_t cols)
 {
   printf("\033[?25l\033[H\033[2J");
@@ -52,7 +65,8 @@ int main(int args, char* argv[])
   }
   char* addrs = parse_maps(maps);
   fclose(maps);
-  struct heap_stack* mem_info = parse_addr(addrs);
+  struct mem_buf* mb = malloc(sizeof(struct mem_buf));
+  mb->mem_info = parse_addr(addrs);
   
   char* mem_path = cat_path(args, argv, "/mem");
   FILE* mem = fopen(mem_path, "r");
@@ -63,34 +77,32 @@ int main(int args, char* argv[])
   }
   
   parse_command(args, argv, FLAG);
-  char *old_buf, *new_buf;
-  old_buf = parse_mem(mem, mem_info, FLAG);
+  mb->old_buf = parse_mem(mem, mb->mem_info, FLAG);
   long int buf_size  = strcmp(FLAG, "stack") == 0 ?
-    mem_info->stack_end - mem_info->stack_start : mem_info->heap_end - mem_info->heap_start;
+    mb->mem_info->stack_end - mb->mem_info->stack_start : mb->mem_info->heap_end - mb->mem_info->heap_start;
   long int row_range = buf_size / (ws->ws_row - 2);
   long int uni_range = row_range / ws->ws_col;
   long int curr_range= uni_range;
   draw_view(ws->ws_row, ws->ws_col);
   while (! stop) {
-    new_buf = strcmp(FLAG, "stack") == 0 ?
-      parse_mem(mem, mem_info, "stack") : parse_mem(mem, mem_info, "heap");
+    mb->new_buf = strcmp(FLAG, "stack") == 0 ?
+      parse_mem(mem, mb->mem_info, "stack") : parse_mem(mem, mb->mem_info, "heap");
     buf_size  = strcmp(FLAG, "stack") == 0 ?
-      mem_info->stack_end - mem_info->stack_start : mem_info->heap_end - mem_info->heap_start;
+      mb->mem_info->stack_end - mb->mem_info->stack_start : mb->mem_info->heap_end - mb->mem_info->heap_start;
     for (int i = 0; i < buf_size; i += uni_range) {
       curr_range = i + uni_range > buf_size ? buf_size - i : uni_range;
-      if (memcmp(old_buf + i, new_buf + i, curr_range) != 0) {
+      if (memcmp(mb->old_buf + i, mb->new_buf + i, curr_range) != 0) {
 	printf("\033[%d;%dH\033[31mX", (i / row_range) + 2, (i % row_range) / uni_range + 1);
       } else {
 	printf("\033[%d;%dH\033[37m|", (i / row_range) + 2, (i % row_range) / uni_range + 1);
       }
     }
-    free(old_buf);
-    old_buf = new_buf;
+    free(mb->old_buf);
+    mb->old_buf = mb->new_buf;
     fflush(stdout);
     usleep(500000);
   }
-  free(new_buf);
-  free(mem_info);
+  free_buf(mb);
   free(ws);
   fclose(mem);
   printf("\033[H\033[J\033[?25h");
